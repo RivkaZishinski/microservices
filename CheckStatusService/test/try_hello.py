180b24d3f72ab9c040788258ce2f6745ee0dd920
R. Zishinski <rivkaz@skyvar.co.il>
2023-12-27 13:47:20 +0200

    2

--- CheckStatusService/test/try_hello.py
+++ CheckStatusService/test/try_hello.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.hello_world import hello_world
+from src.try_hello  import hello_world
 
 @pytest.mark.parametrize("argument_values", [True, 1, 1.0])
 def test_example(argument_values):
@@ -8,3 +8,4 @@ def test_example(argument_values):
 
 def test_hello_world(): 
     assert hello_world() == "Hello World!"
+print(" hello world")    
